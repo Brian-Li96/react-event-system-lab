5d700159835111109552721f74b9ae1e13fc3c06
Brian Li <Brian.Li96@yahoo.com>
2021-02-02 01:04:37 -0600

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
+import React, {Component} from "react";
+
 // Code EyesOnMe Component Here
+export default class EyesOnMe extends Component {
+
+    focusHandle = () => console.log('Good!')
+    blurHandle = () => console.log('Hey! Eyes on me!')
+
+
+    render() {
+        return(
+            <button onFocus={this.focusHandle} onBlur={this.blurHandle} >
+
+            </button>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
+import React, {Component} from "react";
+
 // Code Keypad Component Here
+export default class KeyPad extends Component {
+
+    handleInputPassword = () => console.log('Entering password...')
+
+    render() {
+        return (
+            <div>
+                <input 
+                    type="password"
+                    onKeyUp={this.handleInputPassword}
+                />
+            </div>
+        )
+    }
+}
